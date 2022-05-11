e91d3371c464b495ae997171f14a6569e6d51c1b
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-05-11 22:51:07 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,8 +1,8 @@
 
 def calculateStats(numbers):
   if len(numbers)==0:
-        return float("nan")
+    return float("nan")
         
-    else:
-        return {"avg": sum(numbers)/len(numbers), "max": max(numbers), "min": min(numbers)}
+  else:
+    return {"avg": sum(numbers)/len(numbers), "max": max(numbers), "min": min(numbers)}
         
