b244a86fa5484d5c3e2b7a21caf6d73fd53e5c5d
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-05-11 22:53:23 +0530

    Update statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -8,10 +8,20 @@ class EmailAlert():
 class LEDAlert():
     def __init__(self):
         self.ledGlows = False
-        
+class StatsAlerter():
+    def __init__(self,v1,v2):
+        self.maxThreshold = v1
+        self.list = v2
+    def checkAndAlert(self,v):
+        Inputs = max(v)
+        if Inputs > self.maxThreshold:
+            self.list[0].emailSent = True
+            self.list[1].ledGlows = True
+            
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
-    computedStats = statistics.calculateStats([1.5, 8.9, 3.2, 4.5])
+    Inputs = [1.5, 8.9, 3.2, 4.5]
+    computedStats = statistics.calculateStats(Inputs)
     epsilon = 0.001
     self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
     self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
