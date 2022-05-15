ce757d7cf1fb56a3f7129ba86ebfd2b232b78c11
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-05-15 17:11:44 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,8 +1,8 @@
-import numpy as np
+
 def calculateStats(numbers):
   if len(numbers)==0:
     return {"avg": float('nan'), "max": float('nan'), "min": float('nan')}
         
   else:
-    return {"avg": np.mean(numbers), "max": max(numbers), "min": min(numbers)}
+    return {"avg": sum(numbers)/len(numbers), "max": max(numbers), "min": min(numbers)}
         
